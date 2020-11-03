5ef57fbeba6c2934831b948103f9a1d7c3409209
Omar Saifi <a4pzinty86>
2020-11-03 20:59:55 +0100

    Skapat en ny klass Person och tilldelat metoden inkomst.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
+        Person Omar = new Person();
+        Omar.income = RentCar.personIncome();
+
 
     }
 }
--- src/RentCar.java
+++ src/RentCar.java
@@ -1,3 +1,11 @@
+import javax.swing.*;
+
 public class RentCar {
-    
+    public static int personIncome(){
+
+        int income = Integer.parseInt(JOptionPane.showInputDialog("Vad är din inkomst?"));
+        return income;
+
+
+    }
 }
