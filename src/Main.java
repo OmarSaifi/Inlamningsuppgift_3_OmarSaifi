3f40ebb13afd43bd10bf704a8176f54ff027fdde
Omar Saifi <a4pzinty86>
2020-11-09 20:43:36 +0100

    Har ändrat till en dialogruta istället och finat till.

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Person Omar = new Person();
-        Omar.licence = RentCar.gotLicence();
-        Omar.yearLicence = RentCar.yearLicence();
-        Omar.income = RentCar.personIncome();
+        Person x = new Person();
+        x.licence = RentCar.gotLicence();
+        x.yearLicence = RentCar.yearLicence();
+        x.income = RentCar.personIncome();
 
-        RentCar.canIHire(Omar.income, Omar.yearLicence, Omar.licence);
+        RentCar.canIHire(x.income, x.yearLicence, x.licence);
 
     }
 }
--- src/RentCar.java
+++ src/RentCar.java
@@ -1,55 +1,55 @@
 import javax.swing.*;
 
 public class RentCar {
-
     /**
      * En klassmetod som kollar om jag får hyra en Ferrari.
      *
      */
 
-    public static void canIHire(int income, int yearLicence, boolean licence) {
+    static void canIHire(int income, int yearLicence, boolean licence) {
 
         String message;
 
-        if (licence && income >= 35000 && yearLicence >=3)
+        if (licence && yearLicence >=3 && income >= 35000)
             message = "Du får hyra en Ferrari";
         else
             message = "Du får inte hyra en Ferrari";
-        System.out.println(message);
+        JOptionPane.showMessageDialog(null, message);
     }
     /**
      * En klassmetod som kollar om du har ett körkort.
      *
-     * @returnerar en boolean för körkortet.
+     * @return en boolean för körkortet.
      */
 
-    public static boolean gotLicence() {
+    static boolean gotLicence() {
 
         int license = JOptionPane.showConfirmDialog(null, "Har du körkort?");
         if(license == 0) return true;
         else
             return false;
     }
-
     /**
      * En klassmetod som kollar hur länge du har haft ditt körkort.
      *
-     * @returnear antal år som man har haft körkortet.
+     * @return antal år som man har haft körkortet.
      */
-    public static int yearLicence() {
+    static int yearLicence() {
 
-        int yearLicence = Integer.parseInt(JOptionPane.showInputDialog("Hur många år har du haft ditt körtkort? (Skriv in i siffor ex: 2) och (Har du inget körkort skriv siffra: 0)"));
+        int yearLicence = Integer.parseInt(JOptionPane.showInputDialog
+                ("Hur många år har du haft ditt körtkort? (Skriv in i siffor ex: 2)" +
+                        "\noch" +
+                        "\n(Har du inget körkort skriv siffra: 0)"));
         return yearLicence;
     }
     /**
-     *En klassmetod som kollar vad din inkomst är.
+     * En klassmetod som kollar vad din inkomst är.
      *
-     *@returnerar inkomsten.
+     * @return inkomsten.
      */
-    public static int personIncome() {
+    static int personIncome() {
 
-        int income = Integer.parseInt(JOptionPane.showInputDialog("Vad är din inkomst (före skatt)? Ex: 12000"));
+        int income = Integer.parseInt(JOptionPane.showInputDialog("Vad är din månadsinkomst (före skatt)? Ex: 12000"));
         return income;
-
     }
 }
